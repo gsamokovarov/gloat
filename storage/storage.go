ecddf8588f933ca6d539915213b988079e2eee00
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-04-12 16:06:10 +0300

    Introduce Storage.Remove from the interface

--- storage/storage.go
+++ storage/storage.go
@@ -4,5 +4,6 @@ import "github.com/gsamokovarov/gloat/migration"
 
 type Storage interface {
 	Insert(*migration.Migration) error
+	Remove(*migration.Migration) error
 	All() (migration.Migrations, error)
 }
