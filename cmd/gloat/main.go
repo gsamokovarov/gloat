76a77beb8deb722df8c443e5debe588206d3ce4a
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-04-19 16:44:45 +0300

    Soft-code the hard-coded binary drivers and migration folders

--- cmd/gloat/main.go
+++ cmd/gloat/main.go
@@ -1,6 +1,19 @@
 package main
 
-import "os"
+import (
+	"database/sql"
+	"errors"
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/gsamokovarov/gloat"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
+)
 
 const helpMsg = `Usage gloat: [COMMAND ...]
 
@@ -11,70 +24,154 @@ Commands:
   down          Revert the last applied migration
 
 Options:
-  --help        Show this message
+  -src          The folder with migrations
+                (default $DATABASE_MIGRATIONS" or db/migrations)
+  -url          The database connection URL
+                (default $DATABASE_URL)
+  -help         Show this message
 `
 
-func main() {
-	args := os.Args
+var (
+	urlFlag string
+	srcFlag string
+)
 
-	if len(args) < 2 {
-		Exitf(1, helpMsg)
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, helpMsg)
+		os.Exit(1)
 	}
 
-	switch args[1] {
+	args := parseArguments()
+
+	var cmdName string
+	if len(args) > 0 {
+		cmdName = args[0]
+	}
+
+	var err error
+	switch cmdName {
 	case "up":
-		upCmd()
+		err = upCmd()
 	case "down":
-		downCmd()
+		err = downCmd()
+	default:
+		fmt.Fprintf(os.Stderr, helpMsg)
+		os.Exit(1)
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
 	}
 }
 
-func upCmd() {
+func upCmd() error {
 	gl, err := setupGloat()
 	if err != nil {
-		Exitf(1, "Error: %v\n", err)
+		return err
 	}
 
 	migrations, err := gl.Unapplied()
 	if err != nil {
-		Exitf(1, "Error: %v\n", err)
+		return err
 	}
 
 	appliedMigrations := map[int64]bool{}
 
 	for _, migration := range migrations {
-		Outf("Applying migration: %d...\n", migration.Version)
+		fmt.Printf("Applying migration: %d...\n", migration.Version)
 
 		if err := gl.Apply(migration); err != nil {
-			Exitf(1, "Error: %v\n", err)
+			return err
 		}
 
 		appliedMigrations[migration.Version] = true
 	}
 
 	if len(appliedMigrations) == 0 {
-		Outf("No migrations to apply\n")
+		fmt.Printf("No migrations to apply\n")
 	}
+
+	return nil
 }
 
-func downCmd() {
+func downCmd() error {
 	gl, err := setupGloat()
 	if err != nil {
-		Exitf(1, "Error: %v\n", err)
+		return err
 	}
 
 	migration, err := gl.Current()
 	if err != nil {
-		Exitf(1, "Error: %v\n", err)
+		return err
 	}
 
 	if migration == nil {
-		Exitf(0, "No migrations to apply\n")
+		fmt.Printf("No migrations to apply\n")
+		return nil
 	}
 
-	Outf("Reverting migration: %d...\n", migration.Version)
+	fmt.Printf("Reverting migration: %d...\n", migration.Version)
 
 	if err := gl.Revert(migration); err != nil {
-		Exitf(1, "Error: %v\n", err)
+		return err
 	}
+
+	return nil
+}
+
+func parseArguments() []string {
+	urlDefault := os.Getenv("DATABASE_URL")
+	urlUsage := `database connection url`
+	flag.StringVar(&urlFlag, "url", urlDefault, urlUsage)
+
+	srcDefault := os.Getenv("DATABASE_MIGRATIONS")
+	if srcDefault == "" {
+		srcDefault = "db/migrations"
+	}
+	srcUsage := `the folder with migrations`
+	flag.StringVar(&srcFlag, "src", srcDefault, srcUsage)
+
+	flag.Parse()
+
+	return flag.Args()
+}
+
+func setupGloat() (*gloat.Gloat, error) {
+	if urlFlag == "" {
+		return nil, errors.New("no -url or $DATABASE_URL present")
+	}
+
+	driver, err := guessDriver()
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := sql.Open(driver, urlFlag)
+	if err != nil {
+		return nil, err
+	}
+
+	storage, err := gloat.NewDatabaseStorage(driver, db)
+	if err != nil {
+		return nil, err
+	}
+
+	return &gloat.Gloat{
+		InitialPath: srcFlag,
+
+		Storage:  storage,
+		Source:   gloat.NewFileSystemSource(srcFlag),
+		Executor: gloat.NewExecutor(db),
+	}, nil
+}
+
+func guessDriver() (string, error) {
+	parsed, err := url.Parse(urlFlag)
+	if err != nil {
+		return "", err
+	}
+
+	return parsed.Scheme, nil
 }
