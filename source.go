85d34e4d2c31f93d7a7dae80a83c815fbe69c360
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-04-19 16:48:00 +0300

    Fix a nil dereference in FileSystemSource.Collect

--- source.go
+++ source.go
@@ -5,17 +5,32 @@ import (
 	"path/filepath"
 )
 
+// Source is an interface representing a migrations source.
 type Source interface {
 	Collect() (Migrations, error)
 }
 
+// FileSystemSource is a file system source of migrations. The migrations are
+// stored in folders with the following structure:
+//
+// migrations/
+// └── 20170329154959_introduce_domain_model
+//     ├── down.sql
+//     └── up.sql
 type FileSystemSource struct {
 	MigrationsFolder string
 }
 
+// Collect builds migrations stored in a migrations folder like the following
+// the one below:
+//
+// migrations/
+// └── 20170329154959_introduce_domain_model
+//     ├── down.sql
+//     └── up.sql
 func (s *FileSystemSource) Collect() (migrations Migrations, err error) {
 	err = filepath.Walk(s.MigrationsFolder, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() && path != s.MigrationsFolder {
+		if info != nil && info.IsDir() && path != s.MigrationsFolder {
 			migration, err := MigrationFromPath(path)
 			if err != nil {
 				return err
